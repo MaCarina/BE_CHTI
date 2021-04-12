cdcb3237b54ad4a42c019c37bb43e93553a30d02
Carina Malleville <mallevil@insat.insa-toulouse.fr>
2021-04-12 12:22:56 +0200

    derniere modif

--- PjtKEIL_StepSon/Src/principal.c
+++ PjtKEIL_StepSon/Src/principal.c
@@ -14,6 +14,10 @@ int main(void)
 // Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
 CLOCK_Configure();
 
+	GPIO_Configure(GPIOB, 0, OUTPUT, ALT_PPULL);
+	PWM_Init_ff(TIM3,3,720);
+
+
 Timer_1234_Init_ff(TIM4,6552);
 	//N=91*10^^-6 *72*10^^6=6552
 	
@@ -22,11 +26,9 @@ Active_IT_Debordement_Timer(TIM4,2,callbackSon);//prio 2
 //deb timer 4 = lancement du timer_callback
 	
 // configuration de PortB.1 (PB1) en sortie push-pull
-GPIO_Configure(GPIOB, 1, OUTPUT, OUTPUT_PPULL);
+//GPIO_Configure(GPIOB, 1, OUTPUT, OUTPUT_PPULL);
 
 	
-PWM_Init_ff(TIM3,3,720);
-GPIO_Configure(GPIOB, 0, OUTPUT, ALT_PPULL);
 //============================================================================	
 	
 	
